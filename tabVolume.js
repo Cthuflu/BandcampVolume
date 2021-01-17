9b8d5e521fa4a0dd0ce7893c72f951f96413dd38
Jacob Taylor Pfeiffer <jacob@pfeiffer.codes>
2021-01-17 03:13:44 -0500

    Add volume display, change element generation
    - Add volume display, featuring text input
    - Change element generation to be less verbose, much clearer thanks
      to ES6 multi-line strings
      - This now limits the supported versions of firefox, so updated that
        in the manifest to 51 or above with other features
    - Updated styling slightly to match new feature
    - Rename some variables to be clearer

--- tabVolume.js
+++ tabVolume.js
@@ -6,60 +6,70 @@
 
 const VOLUMESTORE = 'bandcampVolume-volume';
 var volume = localStorage.getItem(VOLUMESTORE);
-var volume = volume ? volume : 0.7;
+volume = volume ? volume : 0.7;
+
 var audioTag = document.getElementsByTagName("audio")[0];
-var properties = {	
-	type: "range",
-	min: 0,
-	max: 1, 
-	step: 0.01,
-	value: volume,
-	id: "VolumeSlider"
-};
+var VolumeSliderPercent, volumeControl; // To store references to our control objects
+
+var extensionRow = makeElement("tr", {id: "BandcampVolumeSlider"});
+var rowTemplate = `
+	<td class="VolumeSliderCell" rowspan="2">
+		<label for="VolumeSliderPercent">Volume</label>
+		<span>
+			<input id="VolumeSliderPercent" type="number" min="0" max="100" step="1"/>
+			%
+		</span>
+	</td>
+	<td colspan="3">
+		<input id="VolumeSlider" type="range" min="0" max="1" step="0.01"/>
+	</td>
+`;
 
 function makeSlider() {
-	console.log("BandcampVolume Loaded: Adding Slider");
-	var volumeControl = document.createElement("input");
+	console.log("BandcampVolume: Adding Slider...");
 
-	for(prop in properties) {
-		// Set properties on our volume control object
-		volumeControl[prop] = properties[prop];
+	// Add template to created element
+	extensionRow.innerHTML = rowTemplate;	
+
+	if (document.getElementById("BandcampVolumeSlider")) { 
+		// Replaces existing item rather than endlessly appending rows on extension reload
+		// Debug only
+		document.getElementById("BandcampVolumeSlider").parentNode.replaceChild(extensionRow, document.getElementById("BandcampVolumeSlider"));
+	} else {
+		// Add element to page
+		document.getElementById("trackInfoInner").children[0].children[0].children[0].appendChild(extensionRow);
 	}
+
+	VolumeSliderPercent = document.getElementById("VolumeSliderPercent");
+	volumeControl = document.getElementById("VolumeSlider");
 	setVolume(volume); // Set volume on load, also save the set volume
 
-	if (document.getElementById("VolumeSlider")) { 
-		// I left this in to not refresh for every change while editing
-		// Debug only
-		document.getElementById("VolumeSlider").parentNode.replaceChild(volumeControl, document.getElementById("VolumeSlider"));
-	} else {
-		// Prepare element to add to page
-		var genRow = document.createElement("tr");
-		var volHold = document.createElement("td");
-		var label = document.createElement("td");
-
-		label.innerText = "Volume"; // TODO: We'll put something else in this box later
-		label.className = "VolumeSliderLabel";
-
-		volHold.appendChild(volumeControl);
-		genRow.appendChild(label);
-		genRow.appendChild(volHold);
-
-		// Add element to page
-		document.getElementById("trackInfoInner").children[0].children[0].children[0].appendChild(genRow);
-	}
+	VolumeSliderPercent.addEventListener("change", function(){setVolume(VolumeSliderPercent.value/100)});
 	volumeControl.addEventListener("input", function(){setVolume(volumeControl.value)});
 	volumeControl.addEventListener("change", function(){setVolume(volumeControl.value)});
 }
 
-function setVolume(someValue) {
-	if (someValue == null) return;
-	audioTag.volume = someValue;
-	saveVolume(someValue);
+function makeElement(tag, options) {
+	return Object.assign(document.createElement(tag), options);
 }
 
-function saveVolume(someValue) {
-	if(someValue == null) return;
-	localStorage.setItem(VOLUMESTORE, someValue);
+function setVolume(num) {
+	if (typeof num === 'undefined') return;
+	audioTag.volume = num;
+	saveVolume(num);
+
+	VolumeSliderPercent.value = percent_string(volume);
+	volumeControl.value = volume;
+}
+
+function percent_string(num) {
+	return `${(num * 100).toFixed(0)}`;
+}
+
+function saveVolume(num) {
+	if(num === null) return;
+	volume = num;
+	localStorage.setItem(VOLUMESTORE, num);
 }
 
 makeSlider();
